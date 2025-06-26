6efd5d9394609aa35ab1103f8862b99d640a44ea
Suryaaa <ssuryakiran83@gmail.com>
2025-06-26 14:52:56 +0530

    Add client.py and server.py for chat functionality (Not integrated with CLI)

--- lantern/server.py
+++ lantern/server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from prompt_toolkit import PromptSession, print_formatted_text
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -10,17 +11,48 @@ server.bind((IP, PORT))
 print("Server started on " + IP + ":" + str(PORT))
 
 server.listen(1)    #For P2P connections, can change it to () for Chat Room
-clients = []
 
-def send_message(user, message):
-    pass
+client, address = server.accept()
+print(address[0] + " connected.")
 
-def client_handler(client):
-    while True:
-        username = client.recv(1024).decode('utf-8')
+session = PromptSession()
+active = True
 
+def message_handler():
+    global active
+    while active:
+        message=client.recv(1024).decode('utf-8')
+        if message == "__EXIT__":
+            print("\nClient has exited the chat. Press Enter to exit.")
+            active = False
+            break
+        else:
+            print_formatted_text(f"Client> {message}")
+
+threading.Thread(target=message_handler, daemon=True).start()
 while True:
-    client, address = server.accept()
-    print(address[0] + " connected.")
-    clients.append(client)
-    threading.Thread(target=client_handler, args=(client,)).start()
+    if not active:
+        break
+    send = session.prompt("Chat> ")
+    if send.startswith("/"):
+        if send == '/exit':
+            client.send("__EXIT__".encode('utf-8'))
+            print("Exiting chat.")
+            active = False
+            break
+        elif send == '/help':
+            print_formatted_text("Available commands:\n  /exit - Exit chat\n  /help - Show help")
+            continue
+        else:
+            print("Unknown command. Type /help for available commands.")
+            continue
+    else:
+        client.send(send.encode('utf-8'))
+
+try:
+    client.shutdown(socket.SHUT_RDWR)
+    server.shutdown(socket.SHUT_RDWR)
+except:
+    pass
+server.close()
+client.close()
